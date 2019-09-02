913e9ad1db09789c25beb49611e40f990e4d9fb7
elvisbkt <47518658+elvisbkt@users.noreply.github.com>
2019-09-02 16:28:38 +0300

    add alternative text for mail

--- ig/email.py
+++ ig/email.py
@@ -11,4 +11,6 @@ def send_welcome_email(name,receiver):
     text_content = render_to_string('email/instaemail.txt',{"name": name})
     html_content = render_to_string('email/instaemail.html',{"name": name})
 
-    
+    msg = EmailMultiAlternatives(subject,text_content,sender,[receiver])
+    msg.attach_alternative(html_content,'text/html')
+    msg.send()
