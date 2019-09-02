7cce30863a765c0de520871e98963de695d315ac
elvisbkt <47518658+elvisbkt@users.noreply.github.com>
2019-09-02 16:53:10 +0300

    add secret key

--- instagram/settings.py
+++ instagram/settings.py
@@ -47,6 +47,12 @@ DATABASES['default'].update(db_from_env)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'syy^*ff&@(kp&g5%&ezfev6_n4q@0brsv3%zqgkw46-+y4p-%j'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
