d3cf38322aad834bea0d18c87fdd348a8d377aa0
elvisbkt <47518658+elvisbkt@users.noreply.github.com>
2019-09-02 16:25:15 +0300

    add forms

---
+++ ig/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from .models import Post
+
+class NewsLetterForm(forms.Form):
+    your_name = forms.CharField(label='First Name',max_length=30)
+    email = forms.EmailField(label='Email')
+
+class NewArticleForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ['editor', 'pub_date']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+
+class RegisterForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    email = forms.EmailField(label='Email')
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    password_repeat = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
